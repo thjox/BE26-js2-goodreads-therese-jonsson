34a1bab56b3451bed18c76fb0fe8374b9086616f
Therese Jonsson <therese.jonsson85@gmail.com>
2026-09-21 18:47:52 +0200

    Cleaning up firebase.js

--- src/modules/firebase.js
+++ src/modules/firebase.js
@@ -2,36 +2,29 @@ const baseURL =
   "https://goodread-2dc1a-default-rtdb.europe-west1.firebasedatabase.app/books";
 
 export async function getAllBooks() {
-  try {
-    const response = await fetch(baseURL + ".json");
-    if (!response.ok) {
-      throw new Error("Fetching books failed");
-    }
+  const response = await fetch(baseURL + ".json");
 
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw error;
+  if (!response.ok) {
+    throw new Error("Fetching books failed");
   }
+
+  return await response.json();
 }
 
 export async function addBook(book) {
-  try {
-    const option = {
-      method: "POST",
-      body: JSON.stringify(book),
-      headers: { "Content-Type": "application/json" },
-    };
-    const response = await fetch(baseURL + ".json", option);
-    if (!response.ok) {
-      throw new Error("Post failed!");
-    }
+  const options = {
+    method: "POST",
+    body: JSON.stringify(book),
+    headers: { "Content-Type": "application/json" },
+  };
 
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw error;
+  const response = await fetch(baseURL + ".json", options);
+
+  if (!response.ok) {
+    throw new Error("Adding book failed");
   }
+
+  return await response.json();
 }
 
 export async function updateBook(id, updates) {
